db9fc4a19f1d63e62d4dc4e204ef7f7d0261c706
Vladimir Moushkov <vlindos@Vladimirs-MacBook-Pro.local>
2019-04-17 21:09:10 +0300

    - fix eol to LF

--- src/lib/utils/utils.ts
+++ src/lib/utils/utils.ts
@@ -1,11 +1,12 @@
 const groupBy = <T>(array: Array<T>, groupingKeyFn: (item: T) => string): { [key: string]: Array<T> } => {
   if (typeof groupingKeyFn !== 'function') {
-    throw new Error("groupBy take a function as only parameter");
+    throw new Error('groupBy take a function as only parameter');
   }
   return array.reduce((result: any, item: any) => {
     let key = groupingKeyFn(item);
-    if (!result[key])
+    if (!result[key]) {
       result[key] = [];
+    }
     result[key].push(item);
     return result;
   }, {});
