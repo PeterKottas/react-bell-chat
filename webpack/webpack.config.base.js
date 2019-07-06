293ec346f472cb730df19384317cce26796c426a
Peter Kottas <petokottas@gmail.com>
2019-07-06 12:46:34 +0200

    Updated deps
    Signed-off-by: Peter Kottas <petokottas@gmail.com>

--- webpack/webpack.config.base.js
+++ webpack/webpack.config.base.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const webpack = require('webpack');
 const CheckerPlugin = require('awesome-typescript-loader').CheckerPlugin;
+const TerserPlugin = require("terser-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
 var isLocalBuild = process.env && process.env.NODE_ENV && process.env.NODE_ENV.trim().toString() == 'local';
 
 module.exports = {
@@ -30,5 +33,34 @@ module.exports = {
             filename: '[file].map', // Remove this line if you prefer inline source maps
             moduleFilenameTemplate: path.relative('../lib', '[resourcePath]') // Point sourcemap entries to the original file locations on disk
         })
-    ]
+    ],
+    mode: isLocalBuild ? "development" : "production",
+    ...(!isLocalBuild
+      ? {
+          optimization: {
+            minimizer: [
+              new TerserPlugin({
+                parallel: true,
+                sourceMap: isLocalBuild,
+                cache: true
+              }),
+              new OptimizeCSSAssetsPlugin({}),
+              new webpack.optimize.MinChunkSizePlugin({ minChunkSize: 100000 })
+            ],
+            splitChunks: {
+              cacheGroups: {
+                /*default: false,
+                vendors: false,*/
+                // vendor chunk
+                /*vendor: {
+                  // sync + async chunks
+                  chunks: "all",
+                  // import file path containing node_modules
+                  test: /node_modules/
+                }*/
+              }
+            }
+          }
+        }
+      : {})
 };
