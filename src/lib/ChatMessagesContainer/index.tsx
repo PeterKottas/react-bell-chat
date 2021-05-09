9c142ae40b16d45b59a9906442277abbcba00bbf
Peter Kottas <petokottas@gmail.com>
2021-05-09 16:15:22 +0200

    Deploy commit

--- src/lib/ChatMessagesContainer/index.tsx
+++ src/lib/ChatMessagesContainer/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import * as classNames from 'classnames';
+import classNames from 'classnames';
 import { ChatMessagesContainerClasses, defaultChatMessagesContainerClasses } from './classes';
 import { ChatMessagesContainerStyles, defaultChatMessagesContainerStyles } from './styles';
 export * from './classes';
